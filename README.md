6adc505db7fd5def46bc39475d429d5c546b1305
David Sims <davidesims@icloud.com>
2017-06-04 10:09:17 -0700

    added menus to electron app

--- README.md
+++ README.md
@@ -33,6 +33,13 @@ Allows different processes to communicate between each other.
 
 IPC Renderer sends message to Electron app and IPC Main receives the message. Electron sends message back to front-end and the front-end receives it with the IPC Renderer.
 
+## Menus
+
+Each menu in the menu bar has a sub menu that contains text called labels.
+When using your own custom menu it will displace the normal Electron menu.
+This will also remove the default key binds in Electron. In Mac OS X the very
+first menu is the name of the application (i.e. Electron).
+
 ## Issues
 
 You will likely need to run electron as sudo if the module does not have proper permissions.
--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, Menu } = require('electron');
 
 let mainWindow;
+
+const menuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'New Todo'
+      },
+      {
+        label: 'Quit',
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+
+// Add extra menu option on Mac OS X
+if (process.platform === 'darwin') {
+  menuTemplate.unshift({});
+}
 
 app.on('ready', () => {
   // spawns new browser window
   mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+
+  // Can use this method to change the menu throughout the application.
+  Menu.setApplicationMenu(mainMenu);
 });
 
 // listens to event from front-end
